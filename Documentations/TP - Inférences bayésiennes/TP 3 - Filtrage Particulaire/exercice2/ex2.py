1e8de8e1db685124414d7a286aa8b81e76a77d2d
Alexandre Chaussard <78101027+AlexandreChaussard@users.noreply.github.com>
2022-01-16 19:56:57 +0100

    Update ex2.py

--- Documentations/TP - Inférences bayésiennes/TP 3 - Filtrage Particulaire/exercice2/ex2.py
+++ Documentations/TP - Inférences bayésiennes/TP 3 - Filtrage Particulaire/exercice2/ex2.py
@@ -1,83 +1,156 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from PIL import Image
+import math
+import scipy.stats
 from fonctions_TP_python import *
 
+# Nombre de particules
 N = 50
+# Nombre de couleurs dans l'histogramme
 N_b = 10
+# Variable de discrimination des particules par l'histogramme de couleur associé
 lamb = 20
+# Valeurs de la matrice de bruit pour la variable aléatoire U
 C1 = 300
 C2 = 300
-F = np.array([[1, 0],
-              [0, 1]])
 
+# On charge la 1ère image
 im, filenames, T, SEQUENCE = lecture_image()
+# On sélectionne la zone à suivre
 zoneAT = selectionner_zone()
 
-q_img, q_kmeans, q = calcul_histogramme(im, zoneAT, N_b)
-print("histo 1")
+# On définit les dimensions de l'image extraite
+h_width = zoneAT[2]
+h_high = zoneAT[3]
 
 
+# Permet d'initialiser les particules
 def init_particles(sigma, mu):
     return sigma * np.random.randn(N, 2) + mu
 
 
-particles = init_particles((300) ** .5, [zoneAT[0], zoneAT[1]])
+# On initialise les particules
+particles = init_particles(math.sqrt(300), [zoneAT[0], zoneAT[1]])
 
 
+# Définie la distance BH entre deux histogrammes
 def D(q, q_prime):
     somme = 0
 
-    for i in range(0, len(q)):
-        somme = (q[i] * q_prime[i]) ** .5
+    for i in range(0, N_b):
+        somme = math.sqrt(q[i] * q_prime[i])
 
-    return (1 - somme) ** .5
+    return math.sqrt(1 - somme)
 
 
+# Loi de y sachant x selon les histogrammes
 def g(q, q_prime):
-    return np.exp(- lamb * D(q, q_prime) ** 2)
+    return math.exp(- lamb * (D(q, q_prime) ** 2))
 
 
+# On calcule l'histogramme de référence, en supposant qu'il n'évolue pas beaucoup lors du suivi
+q_img, q_kmeans, q_histo_original = calcul_histogramme(im, zoneAT, N_b)
+plt.pause(0.01)
+
+
+# Fonction de filtrage particulaire
 def filtrage_particulaire():
-    X_est = particles  # vecteur € R^Nx1
-    w = np.array([1/len(X_est)] * len(X_est)).reshape(1, len(X_est))  # vecteur des poids
+    X_est = particles  # initialisation bruitée, vecteur € R^Nx1
+    estimated_trajectory_X = []  # initialisation du tableau des X estimés
+    estimated_trajectory_Y = []  # initialisation du tableau des Y estimés
+    N_eff_list = [] # initialisation de la liste des N_eff
 
-    rememberZoneAT = zoneAT.copy()
-    zoneAT_matrix = np.zeros((N, 4))
-    zoneAT_matrix[:][2] = rememberZoneAT[2]
-    zoneAT_matrix[:][3] = rememberZoneAT[3]
+    w = np.array([1 / N] * N).reshape(N, 1)  # matrice des poids, initialement uniformes
 
-    print("debug 2")
+    X_t = X_est  # X_0 correspond aux particules initiales
 
+    # On plot les particules
+    for i in range(0, N):
+        plt.plot(X_t[i][0], X_t[i][1], marker='x', color='blue')
+
+    plt.pause(0.01)
+
+    # On parcourt les images enregistrées
     for t in range(1, T):
 
-        indice = np.random.choice(range(0, 50), N, list(w[:, -1]))  # tire N particules selon la pondération w
+        # Application de la loi de transition -> on propage les particules
+        U = math.sqrt(C1) * np.random.randn(N, 2)
+        X_t = X_t + U  # application du modèle
 
-        X_t_moins_1 = X_est[indice, :]
-        X_t = X_t_moins_1 + np.random.randn(N,2) * np.array([C1, C2])   # application du modèle
+        W_t = []  # On prépare la liste des poids à l'instant t
 
-        W_t = []
-
-        img = Image.open((SEQUENCE + filenames[t]))
-        print(SEQUENCE, filenames[t])
-        print("Passé")
+        # On récupère l'image de l'instant t
+        new_img = Image.open((SEQUENCE + filenames[t]))
         for i in range(0, N):
-            zoneAT_matrix[i][0] = X_est[i][0]
-            zoneAT_matrix[i][1] = X_est[i][1]
+            rect_x = X_t[i][0]
+            rect_y = X_t[i][1]
+            q_prime_img, q_prime_kmeans, q_prime_histo = calcul_histogramme(new_img,
+                                                                            [rect_x, rect_y, h_width, h_high],
+                                                                            q_kmeans)
 
-            q_prime_img, q_prime_kmeans, q_prime = calcul_histogramme(img, zoneAT_matrix[i], N_b)
-            W_t.append(g(q, q_prime))
+            w_value = w[i][-1] * g(q_histo_original, q_prime_histo)  # On calcule le poids de la particule
+            W_t.append(w_value)
 
-        W_t = W_t / np.sum(W_t)
-        X_est = np.hstack([X_est, X_t])
+        W_t = np.array(W_t)
+        W_t = W_t / np.sum(W_t)  # On normalise les poids
 
-        w = np.hstack([w, W_t])
+        N_eff_inv = 0 # On prépare le calcul de N_eff
+        for i in range(0, N):
+            w_value = W_t[i]
+            N_eff_inv += w_value**2
 
-        plt.Rectangle(zoneAT[0:2], zoneAT[2], zoneAT[3])
+        N_eff_list.append(1 / N_eff_inv)
 
-        plt.imshow(img)
+        indice = np.random.choice(N, N, p=W_t)  # tire N particules selon la pondération W_t
 
-    return X_est, w  # vecteurs de R^NxT
+        # rééchantillonnage
+        X_t = X_t[indice]  # On récupère depuis les indices tirés un vecteur X_t rééchantillonés
 
-filtrage_particulaire()
+        plt.cla()  # Efface le plot
+        plt.clf()  # Efface la figure
+
+        EX = 0  # Calcul de l'espérance en X
+        EY = 0  # Calcul de l'espérance en Y
+
+        for i in range(0, N):
+            plt.plot(X_t[i][0], X_t[i][1], marker='x', color='blue')
+
+            x_value = X_t[i][0]
+            y_value = X_t[i][1]
+            EX += x_value
+            EY += y_value
+
+        EX = EX / N
+        EY = EY / N
+
+        estimated_trajectory_X.append(EX + h_width/2)
+        estimated_trajectory_Y.append(EY + h_high/2)
+
+        X_est = np.hstack([X_est, X_t])  # On ajoute X_t à la matrice X_estimé en dernière colonne
+
+        W_t = W_t.reshape(W_t.shape[0], 1)
+        w = np.hstack([w, W_t])  # On ajoute W_t à la matrice des poids w
+
+        # Plot les particules, le rectangle et la trajectoire
+        plt.plot(estimated_trajectory_X, estimated_trajectory_Y, color="lime")
+        plt.imshow(new_img)
+        rect = ptch.Rectangle([EX, EY], h_width, h_high, linewidth=2, edgecolor='red', facecolor='None')
+        plt.plot(EX, EY, marker='x', color='lime')
+        currentAxis = plt.gca()
+        currentAxis.add_patch(rect)
+
+        plt.pause(0.01)
+
+    return X_est, w, N_eff_list  # vecteurs de R^NxT
+
+
+X_est, w, N_eff_list = filtrage_particulaire()
+
+abscisse = []
+for i in range(0, len(N_eff_list)):
+    abscisse.append(i)
+
+plt.cla()
+plt.clf()
+
+plt.title("N_eff en fonction de l'étape n du filtrage | lambda = " + str(lamb))
+plt.plot(abscisse, N_eff_list)
+plt.pause(100)
