bea8943bfc40d3802df6aaa0c88c5e2f7ac7c309
AlexandreChaussard <alexandre.chaussard@telecom-sudparis.eu>
2022-03-13 23:31:33 +0100

    Version stable

--- Logiciels/Annotations/App.py
+++ Logiciels/Annotations/App.py
@@ -4,6 +4,7 @@ import PIL.ImageTk
 import time
 import matplotlib.pyplot as plt
 import math
+import numpy as np
 from DataManager import Data
 from ButtonClass import Button
 from Video import VideoObject
@@ -23,6 +24,9 @@ class App:
     def __init__(self, window, window_title, video_path):
         # Create the window
         self.window = window
+        self.window.attributes('-fullscreen', True)
+        self.window.bind("<space>", self.pause)
+        self.window.bind("<Return>", self.addAnnotation)
         self.window.title(window_title)
 
         # open video source
@@ -36,25 +40,26 @@ class App:
         self.file.close()
 
         # Create a canvas that can fit the above video source size
-        self.canvas = tkinter.Canvas(self.window, width=self.vid.width * 0.8, height=self.vid.height * 0.8)
-        self.canvas.pack(padx=2, side=tkinter.LEFT)
+        self.canvas = tkinter.Canvas(self.window, width=self.vid.width, height=self.vid.height)
+        self.canvas.pack(padx=2, pady=2, side=tkinter.RIGHT)
 
         # File
         self.file_selector = TopMenu(window=self.window, app=self)
         self.file_selector.addEntry(label="Sélectionner une vidéo", command=chooseFile)
-        self.file_selector.pack(label="Fichier")
+        # self.file_selector.pack(label="Fichier")
 
         # Mode menu
-        self.modeList = [["Porte-aiguille", self.annot_porteAiguille],
-                         ["Pince", self.annot_pince],
-                         ["Préférence manuelle", self.annot_manuelle],
-                         ["Points/Aiguille", self.annot_points],
-                         ["Fil", self.annot_fil]]
-        self.modeVisual = ["-", "-", "D", "D", "D"]
+        self.modeList = [["Porte-aiguille", self.annot_porteAiguille, "-", ["Aucune option"]],
+                         ["Pince", self.annot_pince, "-", ["Aucune option"]],
+                         ["Préférence manuelle", self.annot_manuelle, "D", ["Droitier", "Gaucher"]],
+                         ["Points/Aiguille", self.annot_points, "D", ["Coup droit", "Revers", "Mixte"]],
+                         ["Fil", self.annot_fil, "D", ["Main de la pince", "Main du porte-aiguille", "Les deux mains"]]]
+        self.modeList = np.array(self.modeList)
+        self.modeVisual = self.modeList[:, 2]
         self.mode = None
         self.mode_selector = TopMenu(window=self.window, app=self)
 
-        for modeName, function in self.modeList:
+        for modeName, function, _, __ in self.modeList:
             self.mode_selector.addEntry(label=modeName, command=function)
 
         self.mode_selector.pack(label="Annotations")
@@ -67,7 +72,7 @@ class App:
         # Vitesse de lecture
         self.speed = tkinter.DoubleVar()
         self.speedScale = tkinter.Scale(self.window, orient='horizontal', from_=0.1, to=4,
-                                        resolution=0.1, tickinterval=2, length=350, variable=self.speed,
+                                        resolution=0.1, tickinterval=2, length=550, variable=self.speed,
                                         label='Vitesse de lecture')
         self.speedScale.set(1)
         self.speedScale.pack()
@@ -75,12 +80,19 @@ class App:
         # Button to pause
         self.pause_button = Button("Pause", self.pause, self.window)
         self.pause_button.pack()
+        # Button to restart
+        self.reload_button = Button("Revenir au début", self.restart, self.window)
+        self.reload_button.pack()
         # Button to annotate
         self.annotation_button = Button("Annoter", self.addAnnotation, self.window)
         self.annotation_button.pack()
-        # Button to annotate
-        self.reload_button = Button("Revenir au début", self.restart, self.window)
-        self.reload_button.pack()
+        # Liste d'option par annotation
+        self.dataOptionList = self.modeList[:, 3]
+        self.dataOptionActiveList = ["Aucune option"]
+        self.dataOption = tkinter.StringVar(self.window)
+        self.dataOption.set(self.dataOptionActiveList[0])
+        self.dataOptionMenu = tkinter.OptionMenu(self.window, self.dataOption, *self.dataOptionActiveList)
+        self.dataOptionMenu.pack()
 
         # plot timeline et tout
         self.figureTimeLine = None
@@ -100,6 +112,9 @@ class App:
 
     def update(self):
         self.updateTimeLine()
+        if self.vid.getCurrentFrameIndex() == self.vid.frame_count:
+            self.window.after(self.delay, self.update)
+            return
         # Get a frame from the video source
         if self.isPaused:
             ret, frame = self.vid.getCurrentView()
@@ -117,7 +132,7 @@ class App:
         timeY = [0, 0]
         timeLineX = [self.vid.getCurrentFrameIndex()]
         timeLineY = [0]
-        self.figureTimeLine = plt.Figure(figsize=(5, 3), dpi=100)
+        self.figureTimeLine = plt.Figure(figsize=(6, 3), dpi=100)
         self.cid = self.figureTimeLine.canvas.mpl_connect('button_press_event', self.onClick)
         self.axesPlot = self.figureTimeLine.add_subplot()
         self.axesPlot.plot(timeX, timeY)
@@ -150,7 +165,7 @@ class App:
 
     def updateVisualData(self):
         i = 0
-        for [modeName, _] in self.modeList:
+        for [modeName, _, __, ___] in self.modeList:
             X, Y = self.data.getAxis(modeName)
             X.sort()
             visual = self.modeVisual[i]
@@ -164,8 +179,11 @@ class App:
                         self.axesPlot.plot(abscisse, paire, visual, color=self.data.colors[i])
                         paire = []
                         abscisse = []
-            self.axesPlot.plot(X, Y, "D", color=self.data.colors[i])
+            self.axesPlot.plot(X, Y, "D", color=self.data.colors[i], label=modeName)
             i += 1
+
+        self.axesPlot.plot([0], [len(self.modeList) + 4], ".", color=[1, 1, 1])
+        self.axesPlot.legend(loc='upper left', fontsize='x-small')
 
     def onClick(self, event):
         time, y = event.xdata, event.ydata
@@ -179,6 +197,14 @@ class App:
         self.updateTimeLine(force=True)
         self.figureTimeLine.canvas.mpl_disconnect(self.cid)
 
+    def indexOfAnnotation(self, modeName):
+        i = 0
+        for mName, _, __, ___ in self.modeList:
+            if mName == modeName:
+                return i
+            i += 1
+        return i
+
     # -------------------------------------------------------#
     #                   Button Interactions                 #
     # -------------------------------------------------------#
@@ -186,24 +212,38 @@ class App:
     def updateTitle(self):
         self.annotation_button_text.set(self.mode)
 
+    def updateOptions(self):
+        index = self.indexOfAnnotation(self.mode)
+
+        self.dataOptionMenu['menu'].delete(0, 'end')  # remove full list
+        self.dataOptionActiveList = self.modeList[index][3]
+        self.dataOption.set(self.dataOptionActiveList[0])
+        for opt in self.dataOptionActiveList:
+            self.dataOptionMenu['menu'].add_command(label=opt, command=tkinter._setit(self.dataOption, opt))
+
     def annot_porteAiguille(self):
         self.mode = "Porte-aiguille"
+        self.updateOptions()
         self.updateTitle()
 
     def annot_fil(self):
         self.mode = "Fil"
+        self.updateOptions()
         self.updateTitle()
 
     def annot_points(self):
         self.mode = "Points/Aiguille"
+        self.updateOptions()
         self.updateTitle()
 
     def annot_manuelle(self):
         self.mode = "Préférence manuelle"
+        self.updateOptions()
         self.updateTitle()
 
     def annot_pince(self):
         self.mode = "Pince"
+        self.updateOptions()
         self.updateTitle()
 
     def snapshot(self):
@@ -211,7 +251,7 @@ class App:
         if ret:
             cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
 
-    def pause(self):
+    def pause(self, event=None):
         if self.vid.getCurrentFrameIndex() == self.vid.frame_count:
             return
         if self.isPaused:
@@ -219,10 +259,10 @@ class App:
         else:
             self.isPaused = True
 
-    def addAnnotation(self):
+    def addAnnotation(self, event=None):
         if self.mode is None:
             return
-        self.data.addData(self.vid.getCurrentFrameIndex(), self.mode, self.dataIndicator)
+        self.data.addData(self.vid.getCurrentFrameIndex(), self.mode, self.dataOption.get())
 
     def restart(self):
         self.vid.video.set(cv2.CAP_PROP_POS_FRAMES, 0)
--- Logiciels/Annotations/DataManager.py
+++ Logiciels/Annotations/DataManager.py
@@ -18,37 +18,38 @@ class Data:
         file = open(self.app.annotation_file_path)
         line = file.readline()
         while line:
-            [modeName, value, moreData] = line.split(";")
-            index = self.indexOfAnnotation(modeName)
+            try:
+                [modeName, value, moreData] = line.split(";")
+            except ValueError:
+                line = file.readline()
+                continue
+            index = self.app.indexOfAnnotation(modeName)
             self.moreData[index] = self.moreData[index] + [[value, moreData]]
             self.annotationList[index] = self.annotationList[index] + [int(re.search(r'\d+', value).group())]
             line = file.readline()
         file.close()
 
-    def indexOfAnnotation(self, modeName):
-        i = 0
-        for mName, _ in self.app.modeList:
-            if mName == modeName:
-                return i
-            i += 1
-        return i
 
     def addData(self, value, modeName, moreData):
-        index = self.indexOfAnnotation(modeName)
+        index = self.app.indexOfAnnotation(modeName)
         self.moreData[index] = self.moreData[index] + [[value, moreData]]
         self.annotationList[index] = self.annotationList[index] + [value]
 
         file = open(self.app.annotation_file_path, 'a+')
         file.write(modeName + ";" + str(value) + ";" + moreData + "\n")
         file.close()
+        self.app.updateTimeLine(force=True)
 
     def removeData(self, index, modeName):
-        i = self.indexOfAnnotation(modeName)
+        i = self.app.indexOfAnnotation(modeName)
         if index >= len(self.annotationList[i]):
             return None
         del self.annotationList[i][index]
+        self.app.updateTimeLine(force=True)
 
     def removeDataAround(self, time, index):
+        if index-1 < 0 or index-1 >= len(self.annotationList):
+            return
         data = self.annotationList[index - 1]
         data.sort()
 
@@ -65,14 +66,15 @@ class Data:
             else:
                 break
             i += 1
-        if distance < 4.5:
+        if distance < 5.5:
             del self.annotationList[index-1][valueIndex]
             self.save()
+            self.app.updateTimeLine(force=True)
 
     def save(self):
         file = open(self.app.annotation_file_path, 'w')
         for i in range(0, len(self.app.modeList)):
-            mode, _ = self.app.modeList[i]
+            mode, _, __, ___ = self.app.modeList[i]
             valueVector = self.annotationList[i]
             moreDataVector = self.moreData[i]
             for k in range(0, len(valueVector)):
@@ -82,17 +84,17 @@ class Data:
         file.close()
 
     def getDataAt(self, index, modeName):
-        i = self.indexOfAnnotation(modeName)
+        i = self.app.indexOfAnnotation(modeName)
         if index >= len(self.annotationList[i]):
             return None
         return self.annotationList[i][index]
 
     def getDataFrom(self, modeName):
-        index = self.indexOfAnnotation(modeName)
+        index = self.app.indexOfAnnotation(modeName)
         return self.annotationList[index]
 
     def getAxis(self, modeName):
-        index = self.indexOfAnnotation(modeName)
+        index = self.app.indexOfAnnotation(modeName)
         data = self.annotationList[index]
         Y = []
         for i in range(0, len(data)):
